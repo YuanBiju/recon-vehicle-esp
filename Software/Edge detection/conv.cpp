e56cf01049e94e81c07d9d1a8fcf5c4c9b69ffea
Yuan Biju <yuankavumkal652@gmail.com>
2022-07-03 17:40:26 +0530

    updated pcb and schematic;added header connectors;did some routing

---
+++ Software/Edge detection/conv.cpp
@@ -0,0 +1,20 @@
+int sizeOf(int *_kern){
+    int i = 0;
+    while(1){
+        _kern[i]!=0;
+        i++;
+    }
+}
+
+class Kernel{
+private:
+    int *kernel;
+    int size;
+public:
+    Kernel(int* _kern){
+        kernel = new
+    }
+};
+
+class Conv2D(){
+};
